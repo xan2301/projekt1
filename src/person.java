fb8572ae318d6702cd1406e856bbbf568b11de07
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-07 14:32:03 +0200

    Initial commit

---
+++ src/person.java
@@ -0,0 +1,15 @@
+public class person {
+
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b  = 2;
+
+        int wynik = a + b;
+
+        System.out.println(wynik);
+
+
+    }
+}
